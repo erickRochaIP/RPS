3d1df99f00c4647496beb8bfc98e15308d7406e0
Erick Rocha <erickfr1011@gmail.com>
2023-08-25 23:56:24 -0300

    Consertar implementacoes de funcoes

--- benchmark_functions.py
+++ benchmark_functions.py
@@ -8,16 +8,16 @@ def zakharov_function(x):
 	somatorio05 = 0
 	somatoriox2 = 0
 	for i in range(D):
-		somatorio05 += 0.5 * x[i]
+		somatorio05 += 0.5 * (i+1) * x[i]
 		somatoriox2 += x[i] ** 2
 	return somatoriox2 + somatorio05 ** 2 + somatorio05 ** 4
 
 # Rosenbrock's Function
 def rosenbrock_function(x):
-	D = len(x) - 1
+	D = len(x)
 	somatorio = 0
-	for i in range(D):
-		somatorio += (100 * (x[i] ** 2 - x[i+1]) ** 2 + (x[i+1] -1) ** 2)
+	for i in range(D-1):
+		somatorio += (100 * (x[i+1] - x[i] ** 2) ** 2 + (x[i] -1) ** 2)
 	return somatorio
 
 # Expanded Schaffer's Functon
@@ -37,30 +37,27 @@ def rastrigin_function(x):
     D = len(x)
     somatorio = 0
     for i in range(D):
-        somatorio += (x[i] ** 2 - 10 * math.cos(2*math.pi*x[i]) + 10)
-    return somatorio
+        somatorio += (x[i] ** 2 - 10 * math.cos(2*math.pi*x[i]))
+    return 10 * D + somatorio
 
 # Levy Function
 def levy_function(x):
-    def wi(xi):
-        return 1 + (xi - 1) / (4)
+    def w(i):
+        return 1 + (x[i] - 1) / (4)
     
-    D = len(x) - 1
+    D = len(x)
     somatorio = 0
-    for i in range(D):
-        somatorio += ((wi(x[i]) - 1) ** 2) * (1 + 10 * math.sin(math.pi * wi(x[i]) - 1) ** 2)
-        somatorio += ((wi(x[D]) - 1) ** 2) * (1 + math.sin(2 * math.pi * wi(x[D])) ** 2)
-    somatorio += math.sin(math.pi * wi(x[0])) ** 2
-    return somatorio
+    for i in range(D-1):
+        somatorio += ((w(i) - 1) ** 2) * (1 + 10 * math.sin(math.pi * w(i) + 1) ** 2)
+    somatorio += ((w(D-1) - 1) ** 2) * (1 + math.sin(2 * math.pi * w(D-1)) ** 2)
+    return math.sin(math.pi * w(0)) ** 2 + somatorio
 
 # Bent Cigar Function
 def bent_cigar_function(x):
     somatorio = 0
     for i in range(1, len(x)):
         somatorio += x[i] ** 2
-    somatorio *= 1e06
-    somatorio += x[0] ** 2
-    return somatorio
+    return x[0] ** 2 + 1e06 * somatorio
 
 # HGBat Function
 def hgbat_function(x):
@@ -75,9 +72,9 @@ def hgbat_function(x):
 
 # High Conditioned Elliptic Function
 def high_conditioned_elliptic_function(x):
-    D = len(x) - 1
+    D = len(x)
     somatorio = 0
-    for i in range(len(x)):
+    for i in range(D):
         somatorio += (1e06) ** ((i)/(D-1)) * x[i] ** 2
     return somatorio
 
@@ -94,13 +91,14 @@ def katsuura_function(x):
 
 # Happycat Function
 def happycat_function(x):
+    D = len(x)
     somatorioquadrado = 0
     somatorio = 0
-    for i in range(len(x)):
+    for i in range(D):
         somatorioquadrado += x[i] ** 2
         somatorio += x[i]
-    raiz = (abs(somatorioquadrado - len(x))) ** (0.25)
-    sobreD = (0.5 * somatorioquadrado + somatorio) / len(x)
+    raiz = (abs(somatorioquadrado - D)) ** (0.25)
+    sobreD = (0.5 * somatorioquadrado + somatorio) / D
     return raiz + sobreD + 0.5
 
 # Expanded Rosenbrocks plus Griewangk Function
@@ -113,33 +111,28 @@ def expanded_rosenbrocks_plus_griewangk_function(x):
 
 # Modified Schwefels Function
 def modified_schwefels_function(x):
-    def g(z):
-        if z > 500:
-            return (500 - z % 500) * math.sin(math.sqrt(abs((500 - z % 500)))) - (z - 500) ** 2 / (1000*len(x))
-        elif z < -500:
-            absz = -z
-            return (absz % 500 - 500) * math.sin(math.sqrt(abs((absz % 500 - 500)))) - (z + 500) ** 2 / (1000*len(x))
-        else:
-            return z * math.sin(math.sqrt(abs(z)))
-    D = len(x) - 1
+    def z(i):
+        return x[i] + 420.9687
+    D = len(x)
     somatorio = 0
-    for i in range(len(x)):
-        somatorio += g(x[i] + 4.209687462275036e002)
+    for i in range(D):
+        somatorio += z(i) * math.sin(math.sqrt(abs(z(i))))
     return 418.9829 * D - somatorio
 
 # Ackley's Function
 def ackleys_function(x):
+    D = len(x)
     somatorioquadrado = 0
     somatoriocos = 0
     for i in range(len(x)):
         somatorioquadrado += x[i] ** 2
         somatoriocos += math.cos(2 * math.pi * x[i])
-    return -20 * math.exp(-0.2*math.sqrt(somatorioquadrado/len(x))) - math.exp(somatoriocos / len(x)) + 20 + math.e
+    return -20 * math.exp(-0.2*math.sqrt(somatorioquadrado/D)) - math.exp(somatoriocos / D) + 10 + math.e
 
 # Discus Function
 def discus_function(x):
     somatorioquadrado = 0
-    for i in range(len(x)):
+    for i in range(1, len(x)):
         somatorioquadrado += x[i] ** 2
     return 1e06 * x[0]**2 + somatorioquadrado
 
@@ -148,19 +141,23 @@ def griewanks_function(x):
     somatorio = 0
     multiplicatorio = 1
     for i in range(len(x)):
-        somatorio += x[i]**2 / 400
+        somatorio += x[i]**2
         multiplicatorio *= math.cos(x[i] / math.sqrt(i+1))
-    return somatorio - multiplicatorio + 1
+    return somatorio / 4000 - multiplicatorio + 1
 
 # Schaffer F7 Function
 def schaffer_f7_function(x):
     def si(xi, xi1):
         return math.sqrt(xi ** 2 + xi1 ** 2)
-    
+    def s(i):
+        return math.sqrt(x[i] ** 2 + x[i+1] ** 2)
+
+    D=len(x)
     somatorio = 0
-    for i in range(len(x) - 1):
-        somatorio += math.sqrt(si(x[i], x[i+1])) * (math.sin(50 * si(x[i], x[i+1]) ** 0.2) + 1)
-    return (somatorio / (len(x) - 1)) ** 2
+    for i in range(D - 1):
+        si = s(i)
+        somatorio += math.sqrt(si) + math.sqrt(si) * (math.sin(50 * si ** 0.2)) ** 2
+    return (somatorio / (D - 1)) ** 2
 
 def adiciona_ruido(f, media = 0, desvio = 1):
     return lambda x: f(x) + np.random.normal(media, desvio)
