ac5ddfff984e533b3c820bca4600aa4d0507c9fe
Erick Rocha <erickfr1011@gmail.com>
2023-09-27 18:58:44 -0300

    #5 - Separar versoes RPS e NelderMead

--- nelder_mead_simplex.py
+++ nelder_mead_simplex.py
@@ -1,12 +1,13 @@
 import math
+import random
 
 import numpy as np
-from scipy.stats import ttest_ind
 
 class PontoAvaliacao:
     
+    # TODO: Eliminar variaveis estaticas para permitir paralelismo
+    #       Implementar classe SimplexContext
     _avals = 0
-    _emax = 5
     _max_avals = 400
     _f = None
       
@@ -16,16 +17,6 @@ class PontoAvaliacao:
     
     def reset_avals():
         PontoAvaliacao._avals = 0
-        
-    # Acessar emax
-    def get_emax():
-        return PontoAvaliacao._emax
-    
-    def set_emax(emax):
-        PontoAvaliacao._emax = max(1, emax)
-    
-    def reset_emax():
-        PontoAvaliacao._emax = 5
         
     # Acessar max_avals
     def get_max_avals():
@@ -50,16 +41,14 @@ class PontoAvaliacao:
     def __init__(self, x, lu):
         self.x = x
         self.inviabilidade = PontoAvaliacao.obter_inviabilidade_ponto(x, lu)
-        self.f_x = PontoAvaliacao.lista_f_x(x) if self.inviabilidade == 0 else math.inf
+        self.f_x = [] if self.inviabilidade == 0 else math.inf
     
     # Avaliar funcao
     def avaliar_funcao(x):
+        if PontoAvaliacao._avals >= PontoAvaliacao._max_avals:
+            raise MaxAvalsError(PontoAvaliacao._avals)
         PontoAvaliacao._avals += 1
         return PontoAvaliacao._f(x)
-    
-    def lista_f_x(x):
-        fs = [PontoAvaliacao.avaliar_funcao(x)]
-        return fs
 
     # Obter inviabilidade
     def obter_inviabilidade_ponto(x, lu):
@@ -68,6 +57,13 @@ class PontoAvaliacao:
     def obter_inviabilidade_var(xi, lui):
         li, ui = lui[0], lui[1]
         return max(0, li - xi) + max(0, xi - ui)
+
+    def criar_ponto_dentro(lu):
+        return tuple([(random.uniform(l, u)) for l, u in lu])
+    
+    def criar_lu_para(x0):
+        m = max(map(abs, x0))
+        return [(-m, m) for i in x0]
 
     # Recebe dois pontos e um coeficiente
     # Retorna um ponto deslocado A + AB*coef
@@ -143,116 +139,57 @@ class PontoAvaliacao:
         # Se os dois sao viaveis
         else:
             R = PontoAvaliacao.confidence_compare(self, other)
-            return R == -1
-    
-    # Realiza Teste de T Student entre duas amostras a b, com confianca alpha
-    # Se a < b retorna 1
-    # Se a = b retorna 0
-    # Se a > b retorna -1
-    def tTest(a, b, alpha = 0.05):
-        if ttest_ind(a, b, alternative="less").pvalue < alpha:
-            return 1
-        if ttest_ind(a, b, alternative="greater").pvalue < alpha:
-            return -1
-        return 0
+            return R == -1 or R == 0
     
     # Compara dois pontos a e b, com viabilidade = 0
     # Se a < b retorna 1
     # Se a = b retorna 0
     # Se a > b retorna -1
     def confidence_compare(a, b):
-        e = math.ceil(1 + (PontoAvaliacao._emax - 1)*((PontoAvaliacao._avals / PontoAvaliacao._max_avals))**(1/3))
-        
-        # Se e for 1, compara um unico valor de f(x) para a e b
-        if e == 1:
-            ma, mb = a.f_x[0], b.f_x[0]
-            if ma < mb:
-                return 1
-            if ma == mb:
-                return 0
-            if ma > mb:
-                return -1
-            
-        # Se e > 1, avalia funcao nos pontos ate que ambos
-        # tenham pelo menos duas avaliacoes
-        while(len(a.f_x) < 2):
+        # Se algum dos pontos nao foi avaliado ainda, avalie
+        if a.f_x == []:
             a.f_x.append(PontoAvaliacao.avaliar_funcao(a.x))
-        while(len(b.f_x) < 2):
+        if b.f_x == []:
             b.f_x.append(PontoAvaliacao.avaliar_funcao(b.x))
-        
-        # Extrair tamanho, media, desvio padrao dos pontos
-        na, ma, sa = len(a.f_x), np.mean(a.f_x), np.std(a.f_x)
-        nb, mb, sb = len(b.f_x), np.mean(b.f_x), np.std(b.f_x)
-        
-        # Realizar um teste t
-        H = PontoAvaliacao.tTest(a.f_x, b.f_x)
-        
-        # Enquanto o teste alegar igualdade e for possivel avaliar algum ponto
-        while(H == 0 and (na < e or nb < e)):
-            # Se for possivel avaliar a e o desvio de a for maior que b
-            #   ou se nao for possivel avaliar b
-            # Entao avalia a
-            if (na < e and sa > sb) or (nb == e):
-                a.f_x.append(PontoAvaliacao.avaliar_funcao(a.x))
-            # Caso contrario, sabemos que nb < e e desvio de b maior que a
-            # Entao avalia b
-            else:
-                b.f_x.append(PontoAvaliacao.avaliar_funcao(b.x))
-                
-            # Extrair tamanho, media, desvio padrao dos pontos
-            na, ma, sa = len(a.f_x), np.mean(a.f_x), np.std(a.f_x)
-            nb, mb, sb = len(b.f_x), np.mean(b.f_x), np.std(b.f_x)
             
-            # Realizar um teste t
-            H = PontoAvaliacao.tTest(a.f_x, b.f_x)
+        ma, mb = a.f_x[0], b.f_x[0]
+        if ma < mb:
+            return 1
+        if ma == mb:
+            return 0
+        if ma > mb:
+            return -1
         
-        # Se o teste acabou em igualdade, compara as medias
-        if H == 0:
-            if ma < mb:
-                return 1
-            if ma == mb:
-                return 0
-            if ma > mb:
-                return -1
-        # Senao retorna o resultado do teste
-        else:
-            return H
-            
-    
-    
-        
-
 
 class Simplex:
 
     def __init__(self, x0, lu, k = 0):
         self.pontos = Simplex.criar_simplex(x0, lu, k)
-        self.ordenar_simplex()
 
-
-    # Recebe o eixo e o ponto inicial
-    # Retorna ponto inicial + base do eixo
-    # se o ponto nao possui o eixo, retorna o proprio ponto
+    # Recebe ponto inicial, limites das variaveis, repeticao do melhor
+    # Retorna um simplex, no qual os pontos sao ponto inicial + bases
+    def criar_simplex(x0, lu, k):
+        return [PontoAvaliacao(p, lu) for p in Simplex.criar_pontos(x0, k)]
+    
+    # Recebe o ponto inicial, repeticao do melhor
+    # Retorna coordenadas do ponto inicial + bases
+    def criar_pontos(x0, k):
+        return [Simplex.criar_ponto(i, x0, k) for i in range(len(x0)+1)]
+    
+    # Recebe i, ponto inicial, repeticao do melhor
+    # Retorna ponto i-esimo do simplex
     def criar_ponto(i, x0, k):
         if i == len(x0):
             return tuple(x0)
         
         xi = list(x0[:])
-        if xi[i] > 0.00025:
-            xi[i] = (1 + 0.05*(1+k))*xi[i]
-        else:
-            xi[i] = 0.00025*(1+k)
-        return xi
-
-    # Recebe o ponto inicial
-    # Retorna as bases e a origem
-    def criar_pontos(x0, k):
-        return [Simplex.criar_ponto(i, x0, k) for i in range(len(x0)+1)]
-
-    # Recebe uma funcao, ponto inicial, limites das variaveis
-    # Retorna um simplex ordenado, no qual os pontos sao ponto inicial + bases
-    def criar_simplex(x0, lu, k):
-        return [PontoAvaliacao(p, lu) for p in Simplex.criar_pontos(x0, k)]
+        xi[i] = xi[i] +1+k
+        
+        return tuple(xi)
+    
+    def gerar_novo_simplex(self, lu, k):
+        x0 = PontoAvaliacao.criar_ponto_dentro(lu)
+        self.pontos = Simplex.criar_simplex(x0, lu, k)
 
     # Recebe um simplex
     # Ordena os pontos do simplex baseado na comparacao de PontoAvaliacao
@@ -264,7 +201,7 @@ class Simplex:
     def extrair_dados(self):
         return self.pontos[-1], self.pontos[-2], self.pontos[0]
     
-    def estagnou(self, eps_x):
+    def estagnou(self, eps_x): #modificar para quando o ponto estagnar, gerar um novo ponto aleatório
         pior, _, melhor = self.extrair_dados()
         norma = PontoAvaliacao.calcular_distancia(pior, melhor)
         return norma <= eps_x
@@ -276,12 +213,19 @@ class Simplex:
         return tuple(sum(p.x[i] for p in self.pontos[:-1]) / n for i in range(n))
 
     # Recebe simplex ordenado, um ponto
-    # Substitui pior ponto do simplex pelo ponto fornecido e ordena o simplex
+    # Substitui pior ponto do simplex pelo ponto fornecido
     def substituir_pior_ponto(self, pa):
-        self.pontos = self.pontos[:-1] + [pa]
-        self.ordenar_simplex()
+        i = 0
+        self.pontos.pop()
+        try:
+            for p in self.pontos:
+                if p > pa:
+                    break
+                i += 1
+        finally:
+            self.pontos.insert(i, pa)
 
-    # Recebe Simplex, coeficiente de encolhimento, funcao e limites das variaveis
+    # Recebe simplex ordenado, coeficiente de encolhimento, funcao e limites das variaveis
     # Encolhe o Simplex e o ordena
     def contrair_simplex(self, coef, lu):
         n = len(self.pontos) - 1
@@ -300,3 +244,10 @@ class Simplex:
             pa.print_ponto()
         print("================")
 
+
+class MaxAvalsError(Exception):
+    def __init__(self, avals=None):
+        self.message = "Quantidade maxima de avaliacoes atingida."
+        if avals is not None:
+            self.message += " (" + str(avals) + " avaliacoes)"
+        super().__init__(self.message)
---
+++ rps_avg.py
@@ -0,0 +1,152 @@
+from rps_avg_simplex import Simplex, PontoAvaliacao, MaxAvalsError
+
+import matplotlib.pyplot as plt
+from matplotlib.animation import PillowWriter
+import numpy as np
+
+# Recebe uma funcao, ponto inicial, quantidade maxima de avaliacoes, limites de variaveis, parametros
+# tolerancia, quantidade maxima de avaliacoes em um ponto
+# Retorna o ponto cujo valor da funcao e o menor encontrado
+def rps_avg(f, x0 = None, lu = None, max_avals = 200,
+        dr = 2, de = 2, dc = 0.5, ds = 0.5, eps_x = 0.0, emax = 5,
+        save_gif = False, titulo = None, f_original = None):
+    coef_reflexao = dr
+    coef_exp = de
+    coef_contracao = dc
+    coef_encolhimento = ds
+    
+    # quantidade de vezes que melhor ponto permanece apos estagnacao
+    k = 0
+    
+    # Set parametros inicias
+    PontoAvaliacao.reset_avals()
+    PontoAvaliacao.set_emax(emax)
+    PontoAvaliacao.set_max_avals(max_avals)
+    PontoAvaliacao.set_f(f)
+    
+    if lu is None and x0 is None:
+        lu = [(-5, 5), (-5, 5)]
+        x0 = PontoAvaliacao.criar_ponto_dentro(lu)
+    elif x0 is None:
+        x0 = PontoAvaliacao.criar_ponto_dentro(lu)
+    elif lu is None:
+        lu = PontoAvaliacao.criar_lu_para(x0)
+                
+    xlists = []
+    ylists = []
+
+    # Criar Simplex
+    S = Simplex(x0, lu)
+    melhor = S.pontos[-1]
+    melhor_de_todos = melhor
+    
+    try:
+        S.ordenar_simplex()
+    
+        pior, segundo_pior, melhor = S.extrair_dados()
+        melhor_de_todos = melhor
+        atual_melhor = melhor
+
+        while PontoAvaliacao.get_max_avals() > PontoAvaliacao.get_avals():
+            if save_gif:
+                xlists.append(get_list(pior, segundo_pior, melhor, 0))
+                ylists.append(get_list(pior, segundo_pior, melhor, 1))
+            
+            if melhor_de_todos > melhor:    # Se forem o mesmo ponto vao gastar avaliacoes
+                melhor_de_todos = melhor    # Talvez comparar de outra forma
+            # Se pior ponto esta muito proximo do melhor, reinicia simplex em volta do melhor
+            if S.estagnou(eps_x):
+                if PontoAvaliacao.calcular_distancia(atual_melhor, melhor) < eps_x:
+                    k += 1
+                else:
+                    k = 0
+                atual_melhor = melhor
+                S.gerar_novo_simplex(lu, k)
+                S.ordenar_simplex()
+                pior, segundo_pior, melhor = S.extrair_dados()
+                continue
+
+            centroide = S.calcular_centroide()
+            
+            # Calcula ponto refletido
+            refletido = PontoAvaliacao.calcular_ponto_deslocado(pior.x, centroide, coef_reflexao)
+            refletido = PontoAvaliacao(refletido, lu)
+            
+            # Se refletido melhor que segundo pior, mas nao melhor que o melhor
+            if segundo_pior > refletido and refletido >= melhor:
+                S.substituir_pior_ponto(refletido)
+                pior, segundo_pior, melhor = S.extrair_dados()
+                continue
+            # Se refletido melhor que o melhor
+            if melhor > refletido:
+                # Calcula expandido, e adiciona o melhor ao simplex
+                expandido = PontoAvaliacao.calcular_ponto_deslocado(centroide, refletido.x, coef_exp)
+                expandido = PontoAvaliacao(expandido, lu)
+                if refletido > expandido:
+                    S.substituir_pior_ponto(expandido)
+                else:
+                    S.substituir_pior_ponto(refletido)
+                pior, segundo_pior, melhor = S.extrair_dados()
+                continue
+            # Se refletido for pior que o pior
+            if pior >= refletido:
+                contraido_externo = PontoAvaliacao.calcular_ponto_deslocado(centroide, refletido.x, coef_contracao)
+                contraido_externo = PontoAvaliacao(contraido_externo, lu)
+                if refletido > contraido_externo:
+                    S.substituir_pior_ponto(contraido_externo)
+                    pior, segundo_pior, melhor = S.extrair_dados()
+                    continue
+            # Se refletido esta entre pior e segundo pior
+            else:
+                contraido_interno = PontoAvaliacao.calcular_ponto_deslocado(centroide, refletido.x, -coef_contracao)
+                contraido_interno = PontoAvaliacao(contraido_interno, lu)
+                if pior > contraido_interno:
+                    S.substituir_pior_ponto(contraido_interno)
+                    pior, segundo_pior, melhor = S.extrair_dados()
+                    continue
+
+            S.contrair_simplex(coef_encolhimento, lu)
+            pior, segundo_pior, melhor = S.extrair_dados()
+            
+    except MaxAvalsError:
+        pass
+    finally:
+        # Reset parametros
+        PontoAvaliacao.reset_avals()
+        PontoAvaliacao.reset_emax()
+        PontoAvaliacao.reset_max_avals()
+        PontoAvaliacao.reset_f()
+    
+    if save_gif:
+        get_gif(lu, xlists, ylists, titulo, f_original)
+
+    return melhor_de_todos
+
+
+def get_gif(lu, xlists, ylists, titulo, f_original=None):
+    fig, ax = plt.subplots()
+    ax.set_title(titulo)
+    plt.xlim(lu[0])
+    plt.ylim(lu[1])
+    metadata = dict(title='Simplex', artist='')
+    writer = PillowWriter(fps=5, metadata=metadata)
+    if f_original is not None:
+        delta = 0.025
+        x = np.arange(lu[0][0], lu[0][1], delta)
+        y = np.arange(lu[1][0], lu[1][1], delta)
+        X, Y = np.meshgrid(x, y)
+        m, n = len(X), len(X[0])
+        Z = [[f_original([X[i][j], Y[i][j]]) for j in range(n)] for i in range(m)]
+        im = ax.imshow(Z, interpolation='bilinear', origin='lower', alpha=0.6,
+               extent=(lu[0][0], lu[0][1], lu[1][0], lu[1][1]))
+        CS = ax.contour(X, Y, Z)
+    
+    l, = plt.plot([], [], 'k-')
+    with writer.saving(fig, "simplex.gif", 100):
+        for (xlist, ylist) in zip(xlists, ylists):
+            l.set_data(xlist, ylist)
+            writer.grab_frame()
+            
+
+def get_list(p, s_p, m, i):
+    return [p.x[i], s_p.x[i], m.x[i], p.x[i]]
