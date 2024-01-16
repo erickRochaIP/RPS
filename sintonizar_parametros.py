ebcbf2bacfc5ece68ef722fe7324e7a36cb7fedc
Erick Rocha <erickfr1011@gmail.com>
2024-01-16 16:11:44 -0300

    Graficos com os resultados do experimento oficial

--- sintonizar_parametros.py
+++ sintonizar_parametros.py
@@ -4,6 +4,10 @@ import random
 import sys
 
 import numpy as np
+
+from matplotlib import pyplot as plt
+plt.rcParams['text.usetex'] = True
+
 from skopt.plots import plot_convergence
 from skopt.space import Real, Integer, Categorical
 from skopt.utils import use_named_args
@@ -70,39 +74,73 @@ space_rps_test = [
 ruidos = [1, 5, 10]
 dims = [10]
 functions = [
-    bf.high_conditioned_elliptic_function,
+    bf.zakharov_function,
     bf.rosenbrock_function,
     bf.expanded_schaffer_function,
-    bf.rastrigin_function
+    bf.rastrigin_function,
+    bf.levy_function,
+    bf.bent_cigar_function,
+    bf.hgbat_function,
+    bf.high_conditioned_elliptic_function,
+    bf.katsuura_function,
+    bf.happycat_function,
+    bf.expanded_rosenbrocks_plus_griewangk_function,
+    bf.modified_schwefels_function,
+    bf.ackleys_function,
+    bf.discus_function,
+    bf.griewanks_function,
+    bf.schaffer_f7_function,
+    ]
+function_labels = [
+    "Zakharov",
+    "Rosenbrock",
+    "Schaffer",
+    "Rastrigin",
+    "Levy",
+    "Bent Cigar",
+    "HGBat",
+    "Elliptic",
+    "Katsuura",
+    "HappyCat",
+    "Rosenbrock+Griewangk",
+    "Schwefel",
+    "Ackley",
+    "Discus",
+    "Griewank",
+    "Schaffer",
     ]
 metodos = [rps, nelder_mead_base, nelder_mead_reset, rps_avg, rps_test]
 algoritmos = ["rps", "nelder_mead_base", "nelder_mead_reset", "rps_avg", "rps_test"]
 spaces = [space_rps, space_nelder_mead_base, space_nelder_mead_reset, space_rps_avg, space_rps_test]
 
 
-for fun in functions:
+for fun, fun_label in zip(functions, function_labels):
     for ruido in ruidos:
         funruido = bf.adiciona_ruido(fun, desvio=ruido)
         for dim in dims:
-            qtd = 1
-            bounds = [(-100, 100)]*dim
-            max_avals = 1000*dim
+            qtd = 5
+            bounds = [(-50, 50)]*dim
+            max_avals = 100*dim
             x0s = [[(random.uniform(l, u)) for l, u in bounds] for _ in range(qtd)]
             for space, metodo, alg in zip(spaces, metodos, algoritmos):
                 
                 @use_named_args(space)
                 def eval(**params):
-                    print(params)
-                    return sum([
+                    # print(params)
+                    return np.median([
                         fun(metodo(f=funruido, x0=x0, max_avals=max_avals, lu=bounds, **params).x)
                         for x0 in x0s
                         ])
                 
-                res_gp = gp_minimize(eval, space, n_calls=30, random_state=0)
+                res_gp = gp_minimize(eval, space, n_calls=100) #, random_state=0
                 params_sintonizados = {s.name: p for s, p in zip(space, res_gp.x)}
                 filename = path + alg + "-" + fun.__name__ + "-" + str(dim) + "-" + str(ruido)
                 with open(filename + "-par.pkl", "wb") as fp:
                     pickle.dump(params_sintonizados, fp)
                 ax = plot_convergence(res_gp)
+                plt.xlabel('Number of evaluations')
+                plt.ylabel('Utility function value')
+                plt.title(fun_label + ': Convergence plot for ' + " $\sigma = $" + str(ruido))
                 ax.figure.savefig(filename + "-conv_plot.png")
+                ax.figure.savefig(filename + "-conv_plot.pdf")
                 ax.figure.clear()
--- testar_convergencia.py
+++ testar_convergencia.py
@@ -4,6 +4,8 @@ import random
 import sys
 
 from matplotlib import pyplot as plt
+plt.rcParams['text.usetex'] = True
+
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -32,19 +34,19 @@ def get_params(alg, f, d, r):
     
     return params
 
-def gerar_grafico_evolucao(amostras_mets, fun, dim, ruido):
+def gerar_grafico_evolucao(amostras_mets, fun, fun_label, dim, ruido):
     for alg, label in zip(algoritmos, labels):
         plt.plot(range(len(amostras_mets[alg])), np.log(np.array(amostras_mets[alg]) + 1), label=label)
     plt.legend()
-    plt.xlabel("Avaliações")
-    plt.ylabel("Valor de objetivo")
-    plt.title(fun.__name__ + "-" + str(dim) + "-" + str(ruido) + " Evolução")
-    filename = path_results + fun.__name__ + "-" + str(dim) + "-" + str(ruido)
+    plt.xlabel("Number of evaluations")
+    plt.ylabel("Median of best objective function values")
+    plt.title(fun_label + " $\sigma = $" + str(ruido))
+    filename = path_results + fun_label + "-D" + str(dim) + "-SD" + str(ruido) + "-evolution"
     plt.savefig(filename + ".pdf")
     plt.savefig(filename + ".png")
     plt.clf()
 
-def gerar_violin_plot(final_sols, fun, dim, ruido):
+def gerar_violin_plot(final_sols, fun, fun_label, dim, ruido):
     data = [np.log(np.array(final_sols[alg]) + 1) for alg in algoritmos]
     #data = [np.array(final_sols[alg])for alg in algoritmos]
 
@@ -60,17 +62,17 @@ def gerar_violin_plot(final_sols, fun, dim, ruido):
     sns.stripplot(data=df, palette=sns.color_palette(), alpha=0.4, size=7)
     for item in ax.collections[num_items:]:
         item.set_offsets(item.get_offsets() + 0.15)
-    sns.boxplot(data=df, width=0.25, showfliers=False, showmeans=True, meanprops=dict(marker='o', markerfacecolor='darkorange', markersize=10, zorder=3),
+    sns.boxplot(data=df, width=0.25, showfliers=False, showmeans=False, meanprops=dict(marker='o', markerfacecolor='darkorange', markersize=10, zorder=3),
     boxprops=dict(facecolor=(0,0,0,0), linewidth=3, zorder=3), whiskerprops=dict(linewidth=3), capprops=dict(linewidth=3), medianprops=dict(linewidth=3))
     plt.legend(frameon=False, fontsize=15, loc='lower left')
     # plt.title('', fontsize=28)
-    plt.title(fun.__name__ + "-" + str(dim) + "-" + str(ruido) + ' Violin Plot')
-    plt.xlabel('Algoritmos')
-    plt.ylabel('Valor de objetivo')
+    plt.title(fun_label + " $\sigma = $" + str(ruido))
+    plt.xlabel('Algorithms')
+    plt.ylabel('Best objective function values')
     # plt.xticks(fontsize=15)
     # plt.yticks(fontsize=15)
     # sns.despine()
-    filename = path_results + fun.__name__ + "-" + str(dim) + "-" + str(ruido) + "-violinplot"
+    filename = path_results + fun_label + "-D" + str(dim) + "-SD" + str(ruido) + "-violinplot"
     plt.savefig(filename + ".pdf")
     plt.savefig(filename + ".png")
     plt.clf()
@@ -83,14 +85,44 @@ def salvar_evolucao(amostras_mets, fun, dim, ruido):
 ruidos = [1, 5, 10]
 dims = [10]
 functions = [
-    bf.high_conditioned_elliptic_function,
+    bf.zakharov_function,
     bf.rosenbrock_function,
     bf.expanded_schaffer_function,
-    bf.rastrigin_function
+    bf.rastrigin_function,
+    bf.levy_function,
+    bf.bent_cigar_function,
+    bf.hgbat_function,
+    bf.high_conditioned_elliptic_function,
+    bf.katsuura_function,
+    bf.happycat_function,
+    bf.expanded_rosenbrocks_plus_griewangk_function,
+    bf.modified_schwefels_function,
+    bf.ackleys_function,
+    bf.discus_function,
+    bf.griewanks_function,
+    bf.schaffer_f7_function,
     ]
-metodos = [rps, nelder_mead_base, nelder_mead_reset, rps_avg, rps_test]
-algoritmos = ["rps", "nelder_mead_base", "nelder_mead_reset", "rps_avg", "rps_test"]
-labels = ["RPStau", "NMbas", "NMrst", "RPSavg", "RPStst"]
+function_labels = [
+    "Zakharov",
+    "Rosenbrock",
+    "Schaffer",
+    "Rastrigin",
+    "Levy",
+    "Bent Cigar",
+    "HGBat",
+    "Elliptic",
+    "Katsuura",
+    "HappyCat",
+    "Rosenbrock+Griewangk",
+    "Schwefel",
+    "Ackley",
+    "Discus",
+    "Griewank",
+    "Schaffer",
+    ]
+metodos = [nelder_mead_base, nelder_mead_reset, rps_avg, rps_test, rps]
+algoritmos = ["nelder_mead_base", "nelder_mead_reset", "rps_avg", "rps_test", "rps"]
+labels = ["NMbas", "NMrst", "RPSavg", "RPStst", "RPStau"]
 
 # params[alg][f_name_][d][r]
 params = {
@@ -107,31 +139,31 @@ params = {
     for alg in algoritmos
 }
 
-opts = {"lu": [(-100, 100)], "qtd": 15, "max_avals": 10000}
+opts = {"lu": [(-50, 50)], "qtd": 30, "max_avals": 10000}
 
 qtd = opts["qtd"]
 max = opts["max_avals"]
 lu = opts["lu"]
-x0s = [[(random.uniform(l, u)) for l, u in lu] for _ in range(qtd)]
 
 
-for fun in functions:
+for fun, fun_label in zip(functions, function_labels):
     for ruido in ruidos:
         funruido = bf.adiciona_ruido(fun, desvio=ruido)
         for dim in dims:
             bounds = lu*dim
+            x0s = [[(random.uniform(l, u)) for l, u in bounds] for _ in range(qtd)]
             max_avals = max*dim
             amostras_mets = {alg: [] for alg in algoritmos}
             final_sols = {alg: [] for alg in algoritmos}
             for metodo, alg in zip(metodos, algoritmos):
                 parametros = params[alg][fun.__name__][dim][ruido]
                 for x0 in x0s:
-                    x, best_sols = metodo(funruido, x0, lu, max_avals, **parametros, f_original = fun)
+                    x, best_sols = metodo(funruido, x0, bounds, max_avals, **parametros, f_original = fun)
                     amostras_mets[alg].append(best_sols)
                     final_sols[alg].append(best_sols[-1])
-                amostras_mets[alg] = np.mean(amostras_mets[alg], axis=0)
+                amostras_mets[alg] = np.median(amostras_mets[alg], axis=0)
                 
-            gerar_grafico_evolucao(amostras_mets, fun, dim, ruido)
-            gerar_violin_plot(final_sols, fun, dim, ruido)
+            gerar_grafico_evolucao(amostras_mets, fun, fun_label, dim, ruido)
+            gerar_violin_plot(final_sols, fun, fun_label, dim, ruido)
             salvar_evolucao(amostras_mets, fun, dim, ruido)
             
